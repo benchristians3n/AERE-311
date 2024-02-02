8ac13533e7446cb84b171b18c96e94bf4814cffa
aeminkel <156874229+aeminkel@users.noreply.github.com>
2024-02-02 17:11:46 -0600

    Update gas_dynamic_calculator.m

--- gas_dynamic_calculator.m
+++ gas_dynamic_calculator.m
@@ -5,4 +5,13 @@ clc;
 R= 287.05;
 C_p = (7/2)*R;
 
+% The temperature and pressure at the stagnation point of a high-speed missile are 942°R and 7.8 atm, respectively. 
+% Calculate the density at this point. Take 1 atm = 2116 lb/ft2. (Round the final answer to four decimal places.)
+% p = ρRT
+ρ = pRT = (7.8 atm)(1 atm)(2116 lb/ft2)(1716 ft⋅lbslug⋅°R)(942°R) =  0.0102 slug/ft3
+
+% Condition 1: At the stagnation point of a high-speed missile where the temperature and pressure are 938°R and 7.8 atm, respectively.
+
+ 
+
 
